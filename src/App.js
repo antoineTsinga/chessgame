7ff13ec04893b22531f0769396e075a8f4850560
antoineTsinga <antoinetsinga@gmail.com>
2024-06-27 03:10:27 +0200

    add analytics

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 import Layout from "./components/Layout/Layout.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="App">
+      <Analytics />
       <Layout />
     </div>
   );
